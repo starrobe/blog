201d2150869f535820f716d39a5e8ac641f1e39b
starrobe <starrobe@163.com>
2026-09-20 21:28:16 +0800

    feat: add syntax highlighting for code blocks

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 export default defineNuxtConfig({
   modules: ['@nuxt/content'],
+  content: {
+    build: {
+      markdown: {
+        highlight: {
+          theme: 'github-light',
+          langs: ['cpp', 'c', 'python', 'bash']
+        }
+      }
+    }
+  },
   css: ['~/assets/css/main.css'],
   nitro: {
     prerender: {
