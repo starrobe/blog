ea3f68c79c2457ecac61c62f5dbc33517e97647b
starrobe <starrobe@163.com>
2026-09-20 22:21:45 +0800

    style: flip back arrow to right

--- pages/posts/[slug].vue
+++ pages/posts/[slug].vue
@@ -86,7 +86,7 @@ onBeforeUnmount(() => {
       <footer class="foot">
         <span class="back">
           <svg class="back-icon" width="14" height="14" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-            <path d="M10 3 L5 8 L10 13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <path d="M6 3 L11 8 L6 13" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
           </svg>
           <NuxtLink to="/" class="back-link">cd ..</NuxtLink>
         </span>
