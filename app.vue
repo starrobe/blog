1b2bec9af1028606c8ec378468d42047976797ce
starrobe <starrobe@163.com>
2026-09-24 20:23:59 +0800

    refactor: clean up logo code and fix viewBox cropping
    - Remove redundant stroke-linejoin, fill on path, transform-origin, dashoffset
    - Expand viewBox to fit the stroke's round caps
    - Fix indentation

--- app.vue
+++ app.vue
@@ -2,8 +2,8 @@
   <div class="site">
     <GrainOverlay />
     <NuxtLink to="/" class="logo" aria-label="回到首页">
-            <svg class="logo-svg" viewBox="929 162 911 1019" fill="none">
-        <path class="logo-path" pathLength="1" d="M 926.0 876.9 C 1161.2 918.5 1713.5 357.9 1813.5 201.4 C 1880.0 24.9 1430.0 455.4 1367.5 803.9 C 1324.4 1043.9 1568.0 753.4 1640.5 681.9 C 1732.5 501.4 1500.5 627.4 1527.0 772.4 C 1551.5 791.4 1640.0 704.4 1688.0 603.9 C 1697.0 591.9 1484.5 1116.4 1481.0 1178.9 C 1544.5 897.9 1586.5 884.9 1578.5 851.9 C 1498.0 863.4 1856.5 718.9 1844.0 745.9" stroke="currentColor" stroke-width="25" stroke-linecap="round" stroke-linejoin="round" fill="none" />
+      <svg class="logo-svg" viewBox="913 148 944 1044" fill="none">
+        <path class="logo-path" pathLength="1" d="M 926.0 876.9 C 1161.2 918.5 1713.5 357.9 1813.5 201.4 C 1880.0 24.9 1430.0 455.4 1367.5 803.9 C 1324.4 1043.9 1568.0 753.4 1640.5 681.9 C 1732.5 501.4 1500.5 627.4 1527.0 772.4 C 1551.5 791.4 1640.0 704.4 1688.0 603.9 C 1697.0 591.9 1484.5 1116.4 1481.0 1178.9 C 1544.5 897.9 1586.5 884.9 1578.5 851.9 C 1498.0 863.4 1856.5 718.9 1844.0 745.9" stroke="currentColor" stroke-width="25" stroke-linecap="round" />
       </svg>
     </NuxtLink>
     <NuxtPage />
